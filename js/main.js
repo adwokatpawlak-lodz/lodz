00622ca3b2794dff5a6d67bb1382f01245453774
dzojstik <adam.poland.89@gmail.com>
2013-09-23 01:31:58 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -1,32 +1,13 @@
-$('div.floating-menu a').click(function() {
-  //alert( "Handler for .click() called." );
-});
-
-/*background image*/
-// $(window).load(function() {    
-	// var theWindow        = $(window),
-	    // $bg              = $(".bg"),
-	    // aspectRatio      = $bg.width() / $bg.height();		    		
-	// function resizeBg() {
-		// if ( (theWindow.width() / theWindow.height()) < aspectRatio ) {
-		    // $bg
-		    	// .removeClass()
-		    	// .addClass('bgheight');
-		// } else {
-		    // $bg
-		    	// .removeClass()
-		    	// .addClass('bgwidth');
-		// }		
-	// }         			
-	// theWindow.resize(resizeBg).trigger("resize");
-// 
+// $('div.floating-menu a').click(function() {
+  // //alert( "Handler for .click() called." );
 // });
 
-$(document).ready(function() {
-   //$('.scroll-menu').localScroll({duration:1000});
-   
-   // initiate page scroller plugin
-	$('body').pageScroller({
-			navigation: '.scroll-menu'
-	});
-});
+
+// $(document).ready(function() {
+   // //$('.scroll-menu').localScroll({duration:1000});
+//    
+   // // initiate page scroller plugin
+	// $('body').pageScroller({
+			// navigation: '.scroll-menu'
+	// });
+// });
